1136e47e136e52e3fbf4c00ee22674a0471d84ff
Echamma <elias.john.chamma@gmail.com>
2024-09-12 02:44:59 -0500

    cut up the login function into smaller pieces since it did too much and wasn't optimal

--- auth.py
+++ auth.py
@@ -8,19 +8,45 @@ class Auth:
     def __init__(self):
         pass
 
-    def login(self, user_name, password):
+    def load_users_from_file(self):
+
         with open('db.json', 'r') as file:
-            users = json.load(file)['users']
+
+            return json.load(file)['users']
+
+
+    def authenticate_user(self,users, user_name, password):
 
         for user in users:
+
             if user['name'] == user_name and user['password'] == password:
-                    accounts = {}
-                    for account in user['accounts']:
-                        accounts[account['account_number']] = Account(account['balance'])
-                    return User(user['name'], accounts)
-                else:
-                    print("name or password not found")
-            else:
-                print("name or password not found")
-            
+
+                return user
+
         return None
+
+
+    def create_user_object(self,user):
+
+        accounts = {}
+
+        for account in user['accounts']:
+
+            accounts[account['account_number']] = Account(account['balance'])
+
+        return User(user['name'], accounts)
+
+
+    def login(self, user_name, password):
+
+        users = self.load_users_from_file()
+
+        user = self.authenticate_user(users, user_name, password)
+
+        if user:
+
+            return self.create_user_object(user)
+
+        else:
+
+            return None
